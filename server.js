12be6694722fbbc849c6afb0ab9336f4ba3026fe
Harold Gonzales <haroldgonzales@yahoo.com>
2023-04-26 22:03:01 -0700

    Created the config and the Db/Schema

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// Import the custom helper methods
+const helpers = require('./utils/helpers');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Incorporate the custom helper methods
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
